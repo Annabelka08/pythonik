2c03c5d57306aa0f5bec933531aebab1cc01c634
Annabelka08 <belyaeva.ann.2008@gmail.com>
2026-09-20 13:41:43 +0300

    Add files via upload

---
+++ lab01/4.py
@@ -0,0 +1,5 @@
+first_room=input("Введите название первой аудитории ")
+second_room=input("Введите название второй аудитории ")
+change=second_room+' '+first_room
+print(first_room, second_room)
+print(change)
