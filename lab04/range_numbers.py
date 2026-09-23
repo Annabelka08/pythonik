72aae87121dd2e6f2f013668fe5da9f418fbf411
Annabelka08 <belyaeva.ann.2008@gmail.com>
2026-09-23 21:09:22 +0300

    Add files via upload

---
+++ lab04/range_numbers.py
@@ -0,0 +1,10 @@
+a= int(input("Введите число a "))
+b= int(input("Введите число b "))
+if a<b:
+    for i in range(a,b+1):
+        print(i)
+elif a>b:
+    for i in range(a,b+1, -1):
+        print(i)
+else:
+    print(a)
